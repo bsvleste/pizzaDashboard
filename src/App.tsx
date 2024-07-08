b67de78f5b2f211938cf4606c946068239f38c47
bsvleste <bvaleiro@gmail.com>
2024-07-08 16:52:57 -0300

    feature:menu drop and theme

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import './global.css'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router'
 
+import { ThemeProvider } from './components/theme/theme-provider'
 import { Toaster } from './components/ui/toaster'
 import { router } from './routes'
 export function App() {
   return (
-    <HelmetProvider>
-      <Helmet titleTemplate="%s |pizza.shop" />
-      <RouterProvider router={router} />
-      <Toaster />
-    </HelmetProvider>
+    <ThemeProvider storageKey="pizzaShop-theme" defaultTheme="dark">
+      <HelmetProvider>
+        <Helmet titleTemplate="%s |pizza.shop" />
+        <RouterProvider router={router} />
+        <Toaster />
+      </HelmetProvider>
+    </ThemeProvider>
   )
 }
